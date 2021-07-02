01b479eb091c70c91ce909de9f101d232ea8fcd5
dudasoares <dudaserpacamboim@gmail.com>
2021-07-02 00:49:11 -0300

    adding API key

--- webConsumingMovieDB/ViewController.swift
+++ webConsumingMovieDB/ViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+//API key: ed59a401ccb87b2fa3fd6a859f9563c4
+//Token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDU5YTQwMWNjYjg3YjJmYTNmZDZhODU5Zjk1NjNjNCIsInN1YiI6IjYwZGU4NDQ1NGNhNjc2MDA0NjE4MTVjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kAAOj9FWmTPVIyf9OPLLW4SUw0dCLmRQcklMs_08-58
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
